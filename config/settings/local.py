19bfe5ccc2881e6587dd72bbcf45b703a8d3f390
kcalifdimy <kcalifikz@gmail.com>
2022-05-29 12:12:35 -0400

    template file changes

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
     default="MiR0zU2gnVVglE5O2x3SDDBUi51hiLCbJkKMHRYzgwlWIaJBgo9FEhwm4qn31kCm",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['ibmnappstage.herokuapp.com']
+ALLOWED_HOSTS = ['ibmnappstage.herokuapp.com', '0.0.0.0']
 
 # CACHES
 # ------------------------------------------------------------------------------
@@ -44,11 +44,11 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-#if env("USE_DOCKER") == "yes":
- #   import socket
+if env("USE_DOCKER") == "yes":
+    import socket
 
-  #  hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-   # INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 # django-extensions
 # ------------------------------------------------------------------------------
