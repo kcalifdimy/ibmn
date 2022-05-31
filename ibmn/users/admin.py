fff778a106bfbc61d28c9d7b3d01dda5d1385595
kcalifdimy <kcalifikz@gmail.com>
2022-05-31 00:54:23 -0400

    worked on users file

--- ibmn/users/admin.py
+++ ibmn/users/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-
-
+from django.contrib.auth.admin import UserAdmin
 from ibmn.users.models import User
 
 
+ADDITIONAL_USER_FIELDS = (
+    (None, {'fields': ('first_name','last_name','email', )}),)
 
-admin.site.register(User)
 
-# Register your models here.
+class MyUserAdmin(UserAdmin):
+    model = User
+
+    add_fieldsets = UserAdmin.add_fieldsets + ADDITIONAL_USER_FIELDS
+    list_display = ('first_name','last_name','email','gender')
+    list_editable = ('gender',)
+
+
+admin.site.register(User, MyUserAdmin)
--- ibmn/users/models.py
+++ ibmn/users/models.py
@@ -1,19 +1,21 @@
 import uuid
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from ibmn.users.managers import CustomUserManager
+from django.contrib.auth.models import PermissionsMixin
+
+
+#from ibmn.users.managers import CustomUserManager
 
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
     email = models.EmailField(unique=True, null=True, default=None)
     gender = models.CharField(null=True, max_length=10)
+    #is_staff = models.BooleanField(default=True) 
 
-
-    
-
+    #@property
+    #def is_staff(self):
+     #   return self.is_staff
 
     def __str__(self):
         return f'{self.email}'
